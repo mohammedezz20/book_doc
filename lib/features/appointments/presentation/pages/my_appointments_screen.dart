81b103aa49b128e66e1d222d45920a0494de4569
Younes <alaay725@gmail.com>
2024-08-17 18:25:04 +0300

    - finished app layout screen UI and logic. - initiate home, my appointments, search and profile screens. added some asset images.

---
+++ lib/features/appointments/presentation/pages/my_appointments_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyAppointmentsScreen extends StatelessWidget {
+  const MyAppointmentsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Welcome to My Appointments Screen!'),
+      ),
+    );
+  }
+}
