a803908aa8cbe49ad57db8e543d7a7dcbabcb18d
Younes <alaay725@gmail.com>
2024-09-24 18:25:49 +0300

    - remove bloc provider widget from Home screen. - delete checkUserExists method from auth firebase file. - modified signIn with google method in login cubit file . - modified complete profile method in Completeprofile cubit file . - modified Language cubit file.

---
+++ lib/main_production.dart
@@ -0,0 +1,25 @@
+import 'package:book_doc/book_doc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'core/bloc_observer/bloc_observer.dart';
+import 'core/di/service_locator.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setUpServiceLocator();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
+  Bloc.observer = MyBlocObserver();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const BookDoc());
+}
